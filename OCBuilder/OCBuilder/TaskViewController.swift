af17d563ee400ee3c7cc6e29ec8b61eeed13bc95
Pavo <brockbackups@gmail.com>
2019-07-27 22:07:14 -0400

    Added new VC for main Window

---
+++ OCBuilder/OCBuilder/TaskViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TaskViewController.swift
+//  OCBuilder
+//
+//  Created by Henry Brock on 7/27/19.
+//  Copyright © 2019 Pavo. All rights reserved.
+//
+
+import Cocoa
+
+class TaskViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
